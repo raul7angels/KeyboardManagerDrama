7b6363dba41c868828d85d5b2844cf4a336e7853
Unknown <raul@sevenangels.com.au>
2017-10-31 15:25:09 +0700

    first version

---
+++ KeyboardManagerDrama/KeyboardManager.swift
@@ -0,0 +1,9 @@
+//
+//  KeyboardManager.swift
+//  KeyboardManagerDrama
+//
+//  Created by Paiste Family on 31/10/17.
+//  Copyright © 2017 Bam Bam Labs. All rights reserved.
+//
+
+import Foundation
